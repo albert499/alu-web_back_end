57d82ebefd7cd425ce1342d355368f57983d575e
albert499 <a.logozaga@alustudent.com>
2025-10-27 18:20:43 +0000

    ES6_classes

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,7 +1,12 @@
-class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
-    if (typeof year !== 'number') throw new TypeError('Year must be a number');
-    if (typeof location !== 'string') throw new TypeError('Location must be a string');
+    if (typeof year !== 'number') {
+      throw new TypeError('Year must be a number');
+    }
+
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
 
     this._year = year;
     this._location = location;
@@ -16,10 +21,16 @@ class HolbertonClass {
   }
 }
 
-class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-    if (typeof firstName !== 'string') throw new TypeError('First name must be a string');
-    if (typeof lastName !== 'string') throw new TypeError('Last name must be a string');
+    if (typeof firstName !== 'string') {
+      throw new TypeError('First name must be a string');
+    }
+
+    if (typeof lastName !== 'string') {
+      throw new TypeError('Last name must be a string');
+    }
+
     if (!(holbertonClass instanceof HolbertonClass)) {
       throw new TypeError('Holberton class must be a Holberton type');
     }
@@ -54,4 +65,3 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 const listOfStudents = [student1, student2, student3, student4, student5];
 
 export default listOfStudents;
-
