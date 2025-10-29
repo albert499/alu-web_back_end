329d92e624f263a82dc55e969abad5fcd18ad3c7
albert499 <a.logozaga@alustudent.com>
2025-10-29 23:01:31 +0000

    i18n

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -27,7 +27,7 @@ def get_locale():
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
-    
+
     # Fall back to best match from request headers
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
